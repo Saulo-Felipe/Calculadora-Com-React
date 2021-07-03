14795f62bd7a6b8237e4ef8c802c8a02aabacbeb
Saulo Felipe <saulofelipe234567@gmail.com>
2021-07-02 22:51:48 -0300

    modify packager.json

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+<h1>teste</h1>
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+const express = require('express') 
+const favicon = require('express-favicon') 
+const path = require('path') 
+
+const app = express() 
+
+app.use(favicon(__dirname + '/build/favicon.ico')) 
+
+app.use(express.static(__dirname)) 
+
+app.use(express.static(path.join(__dirname, 'build'))) 
+
+//Routes
 app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+  return res.send('pong') 
+})
+
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+  res.sendFile(path.join(__dirname, 'build', 'index.html')) 
+}) 
+
+app.listen(process.env.PORT || 3000, (error) => {
+  if (error) return console.log('Error: '+error)
+
+  console.log('Tudo funcionando Perfeitamente.')
+})
